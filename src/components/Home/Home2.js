d787c364680469577662867e95eee7d88535b5b1
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2024-07-11 19:29:11 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -38,7 +38,7 @@ function Home2() {
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
+              Whenever possible, I also apply my passion for developing projects
               with <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Tanujairam-TG"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/mr.anonymous.wiz"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
